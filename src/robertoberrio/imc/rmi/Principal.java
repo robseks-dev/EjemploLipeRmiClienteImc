f109983627f38c12f5cdf43a9829adfb46e9ced2
robseks-dev <robertocarlosberrio@gmail.com>
2024-09-06 21:57:41 -0500

    Commit add files

---
+++ src/robertoberrio/imc/rmi/Principal.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package robertoberrio.imc.rmi;
+
+import robertoberrio.imc.rmi.vistas.VentanaPrincipal;
+
+/**
+ *
+ * @author rober
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        VentanaPrincipal v = new VentanaPrincipal();
+        v.setLocationRelativeTo(null);
+        v.setVisible(true);
+    }
+    
+}
